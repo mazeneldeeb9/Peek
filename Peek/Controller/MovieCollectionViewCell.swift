7e5a668e9fd211f5790c50588bfd09d19529e930
mazeneldeeb9 <mazeneldeeb9@gmail.com>
2024-08-09 19:10:37 +0300

    implement main view ui

---
+++ Peek/Controller/MovieCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionViewCell.swift
+//  Peek
+//
+//  Created by mazen eldeeb on 09/08/2024.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
